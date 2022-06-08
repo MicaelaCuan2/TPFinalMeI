6de3f6523be1bd38697701dcd89e26e86521c2c6
MicaelaCuan2 <101810478+MicaelaCuan2@users.noreply.github.com>
2022-06-07 19:30:22 -0700

    Agregue camara y panel inicio

---
+++ Assets/Scripts/BotonIniciar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonIniciar : MonoBehaviour
+{
+    public GameObject cartel;
+    public GameObject Texto;
+    public GameObject Boton;
+
+    Text textoCartel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cartel.SetActive(true);
+        Texto.SetActive(true);
+        Boton.SetActive(true);
+    }
+
+    public void BotonInicio()
+    {
+        cartel.SetActive(false);
+        Texto.SetActive(false);
+        Boton.SetActive(false);
+    }
+}
