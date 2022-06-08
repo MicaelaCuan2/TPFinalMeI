30b593da116385dd5e8f155cb4e37aabec4aae62
MicaelaCuan2 <101810478+MicaelaCuan2@users.noreply.github.com>
2022-06-07 18:43:39 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerCollison.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollison : MonoBehaviour
+{
+    public Vector3 originalPosition;
+    Rigidbody myrigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = originalPosition;
+        originalPosition = new Vector3(3, 0.5f, 8);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Pared")
+        {
+            gameObject.transform.position = originalPosition;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.name == "Base")
+        {
+            gameObject.transform.position = originalPosition;
+            myrigidbody.velocity = Vector3.zero;
+            myrigidbody.angularVelocity = Vector3.zero;
+        }
+        if (col.gameObject.name == "Enemigo")
+        {
+            gameObject.transform.position = originalPosition;
+            myrigidbody.velocity = Vector3.zero;
+            myrigidbody.angularVelocity = Vector3.zero;
+        }
+        if (col.gameObject.name == "Enemigo1")
+        {
+            gameObject.transform.position = originalPosition;
+            myrigidbody.velocity = Vector3.zero;
+            myrigidbody.angularVelocity = Vector3.zero;
+        }
+        if (col.gameObject.name == "Enemigo2")
+        {
+            gameObject.transform.position = originalPosition;
+            myrigidbody.velocity = Vector3.zero;
+            myrigidbody.angularVelocity = Vector3.zero;
+        }
+        if (col.gameObject.name == "Enemigo3")
+        {
+            gameObject.transform.position = originalPosition;
+            myrigidbody.velocity = Vector3.zero;
+            myrigidbody.angularVelocity = Vector3.zero;
+        }
+        if (col.gameObject.name == "Enemigo4")
+        {
+            gameObject.transform.position = originalPosition;
+            myrigidbody.velocity = Vector3.zero;
+            myrigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocidad = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(velocidad, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(velocidad, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0, 0, velocidad);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0, velocidad);
+        }
+    }
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            Debug.Log("Saliste, sos capo!");
+        }
+    }
+}
