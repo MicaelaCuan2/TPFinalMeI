83b0ab50b4ad169e3c1ffc1844b64dd2c356ef87
MicaelaCuan2 <101810478+MicaelaCuan2@users.noreply.github.com>
2022-06-08 19:17:05 -0700

    cambie algo en le codigo

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text txtTiempo;
+    public Text txtTimeFloored;
+    public Text txtCountDown;
+
+    public float TimeToChange;
+    public float waitTime;
+    int counter;
+
+    void Start()
+    {
+        counter = 3;
+        txtCountDown.text = counter.ToString();
+    }
+
+    void Update()
+    {
+        txtTiempo.text = Time.time.ToString();
+        txtTimeFloored.text = Mathf.FloorToInt(Time.time).ToString();
+
+        if (TimeToChange < Time.time)
+        {
+            counter--;
+            if (counter != 0)
+            {
+                txtCountDown.text = counter.ToString();
+                TimeToChange += waitTime;
+            }
+            else
+            {
+                txtCountDown.text = "GO!";
+            }
+        }
+    }
+}
