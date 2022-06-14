09c2dec51f13b2237408cc8c7162e0d9ab525826
MicaelaCuan2 <micucuan@gmail.com>
2022-06-14 15:49:40 -0300

    Casi casi terminadooo

--- Assets/Scripts/Instantiator.cs
+++ Assets/Scripts/Instantiator.cs
@@ -11,17 +11,27 @@ public class Instantiator : MonoBehaviour
 
     void Start()
     {
-        int num = 0;
-        while (num < numero)
-        {
-            clon = Instantiate(objectToClone);
-            num++;
-        }
-        Destroy(clon, 1);
+        //int num = 0;
+        //while (num < numero)
+        //{
+        //    clon = Instantiate(objectToClone);
+        //    num++;
+        //}
+        //Destroy(clon, 1);
+        //A
     }
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.Return))
+        {
+            int num = 0;
+            while (num < numero)
+            {
+                clon = Instantiate(objectToClone);
+                num++;
+            }
+            Destroy(clon, 2.5f);
+        }
     }
 }
--- Assets/Scripts/PlayerCollison.cs
+++ Assets/Scripts/PlayerCollison.cs
@@ -6,15 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCollison : MonoBehaviour
 {
-    AudioSource source;
-    public AudioClip Dead, Win;
     public Vector3 originalPosition;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.transform.position = originalPosition;
         originalPosition = new Vector3(9, 0.5f, 18);
-        source = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter(Collision col)
@@ -22,14 +19,10 @@ public class PlayerCollison : MonoBehaviour
         if (col.gameObject.tag == "Muerte")
         {
             gameObject.transform.position = originalPosition;
-            source.clip = Dead;
-            source.Play();
             SceneManager.LoadScene("Perdiste");
         }
         if (col.gameObject.name == "Puerta")
         {
-            source.clip = Win;
-            source.Play();
             SceneManager.LoadScene("Ganaste");
         }
     }
@@ -38,8 +31,6 @@ public class PlayerCollison : MonoBehaviour
         if (col.gameObject.name == "Base")
         {
             gameObject.transform.position = originalPosition;
-            source.clip = Dead;
-            source.Play();
             SceneManager.LoadScene("Perdiste");
         }
     }
